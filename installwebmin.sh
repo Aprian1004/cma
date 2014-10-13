3b37c9d9dd771b9382253de0152d4339a36a85b0
cmaimu <cmdimu@gmail.com>
2014-10-14 03:23:21 +0700

    Create installwebmin.sh

---
+++ installwebmin.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Link DL: 
+
+#update repo:
+deb http://download.webmin.com/download/repository sarge contrib >> /etc/apt/sources.list
+deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib >> /etc/apt/sources.list
+
+#Download dan Instal kunci GPG dengan repository
+cd
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc
+
+#update sistem lagi
+apt-get update
+
+#install webmin
+apt-get install webmin
+
+#Hilangkan SSL
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+sed -i 's/SSL=1/SSL=0/g' /etc/webmin/miniserv.conf
+service webmin restart
+
