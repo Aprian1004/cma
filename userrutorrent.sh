af536a6bd54291bcbf29088cfadc327f14f6bbb8
cmaimu <cmdimu@gmail.com>
2014-10-16 22:16:27 +0700

    Create userrutorrent.sh

---
+++ userrutorrent.sh
@@ -0,0 +1,7 @@
+##!/bin/bash
+#echo "=============================="
+#echo "Pastikan user sudah ada di VPS"
+#echo "=============================="
+#echo -n "Nama user rutorent? "
+read tambah
+#htpasswd -c /var/www/rutorrent/.htpasswd $tambah
