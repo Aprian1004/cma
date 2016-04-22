23cfcc62e1c83db3690c96a5eea4f7d616a118f9
cmaimu <cmdimu@gmail.com>
2016-04-22 10:12:03 +0700

    Update installnginx.sh

--- installnginx.sh
+++ installnginx.sh
@@ -6,8 +6,8 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
 
-curl http://script.jualssh.com/nginx.conf > /etc/nginx/nginx.conf
-curl http://script.jualssh.com/vps.conf > /etc/nginx/conf.d/vps.conf
+curl https://raw.githubusercontent.com/cmaimu/debian7/master/nginx.conf > /etc/nginx/nginx.conf
+curl https://raw.githubusercontent.com/cmaimu/debian7/master/vps.conf > /etc/nginx/conf.d/vps.conf
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 
 mkdir -p /home/vps/public_html
